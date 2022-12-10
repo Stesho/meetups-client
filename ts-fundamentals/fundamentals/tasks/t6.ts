239eae4430fcd0ccf6c8b92661e7a7ab1faaf432
Dmitry Steshenko <3301297@gmail.com>
2022-12-11 01:02:49 +0300

    feat: implement t6 task

--- ts-fundamentals/fundamentals/tasks/t6.ts
+++ ts-fundamentals/fundamentals/tasks/t6.ts
@@ -1,5 +1,13 @@
 // Create interface Array<T> with following arrays method: pop, push, sort, indexOf, every, map, filter, reduce.
 
-interface Array<T> {
+interface Arr<T> {
   slice(start?: number, end?: number): T[];
+  pop(): T;
+  push(value: T): number;
+  sort(callback: (a: T, b: T) => number): T[];
+  indexOf(value: T, fromIndex: number): number;
+  every(callback: (element: T, index: number, array: T[]) => boolean, thisArg: object): boolean;
+  map(callback: (element: T, index: number, array: T[]) => any, thisArg: object): any;
+  filter(callback: (element: T, index: number, array: T[]) => boolean, thisArg: object): T[];
+  reduce(callback: (accumulator: any, currentValue: T, index: number, array: T[]) => boolean, initialValue: any): any;
 }
