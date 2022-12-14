dad6c03e5a44d5f3d4512e8492059e301754f03b
Dmitry Steshenko <3301297@gmail.com>
2022-12-14 19:38:48 +0300

    fix: allow multiple arguments for 'push' funtion for t6 task

--- ts-fundamentals/fundamentals/tasks/t6.ts
+++ ts-fundamentals/fundamentals/tasks/t6.ts
@@ -3,7 +3,7 @@
 interface Arr<T> {
   slice(start?: number, end?: number): T[];
   pop(): T | undefined;
-  push(value: T | T[]): number;
+  push(...value: T[]): number;
   sort(callback: (a: T, b: T) => number): T[];
   indexOf(value: T, fromIndex: number): number;
   every(
