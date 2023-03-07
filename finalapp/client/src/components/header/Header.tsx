1fdd8b064b7ba08a3d2505d0a08ea251413eb959
Dmitry Steshenko <3301297@gmail.com>
2023-03-07 02:36:08 +0000

    fix: burger menu

--- finalapp/client/src/components/header/Header.tsx
+++ finalapp/client/src/components/header/Header.tsx
@@ -1,76 +1,35 @@
 import React from 'react';
 import styles from './Header.module.scss';
 import logo from '../../assets/images/Logo_SaM.svg';
-import Button from '../ui/button/Button';
-import { useNavigate, NavigateFunction } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
-import { ProfileInfo } from '../profile/profileInfo/ProfileInfo';
-import { observer } from 'mobx-react-lite';
-import TranslatedMessage from '../translatedMessage/TranslatedMessage';
-import Translation from '../../core/utils/translation';
-import AccountMenu from '../accountMenu/AccountMenu';
-import LangSwitcher from '../ui/langSwitcher/LangSwitcher';
-import { useStore } from '../../context/storeContext';
+import Navigation from './navigation/Navigation';
 import BurgerMenu from './burgerMenu/BurgerMenu';
 
-export const Header = observer((): JSX.Element => {
-  const userStore = useStore('UserStore');
-  const navigate: NavigateFunction = useNavigate();
+export const Header = (): JSX.Element => {
+  const [isBurger, setIsBurger] = React.useState<boolean>(false);
 
-  const toAuthorizePage = () => {
-    navigate('/authorize');
-  };
+  const checkIsBurger = () => {
+    const pageWidth = document.documentElement.scrollWidth;
+    if(pageWidth <= 768) {
+      setIsBurger(true);
+    }
+    else {
+      setIsBurger(false);
+    }
+  }
+
+  React.useEffect(() => {
+    window.addEventListener("resize", checkIsBurger);
+    return () => {
+      window.removeEventListener("resize", checkIsBurger);
+    };
+  }, []);
 
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
         <img className={styles.logo} src={logo} alt="SaM Solutions logo" />
-        <BurgerMenu />
-        {/* <nav className={styles.navigation}>
-          <NavLink
-            to="/meetups"
-            className={(active) =>
-              `${styles.navItem} ${active.isActive && styles.activeLink}`
-            }
-          >
-            <TranslatedMessage
-              message={Translation.translatedText('meetups.title')}
-            />
-          </NavLink>
-          <NavLink
-            to="/news"
-            className={(active) =>
-              `${styles.navItem} ${active.isActive && styles.activeLink}`
-            }
-          >
-            <TranslatedMessage
-              message={Translation.translatedText('news.title')}
-            />
-          </NavLink>
-        </nav>
-        <div className={styles.mainBar}>
-          <LangSwitcher
-            switcherClassName={styles.langSwitcher}
-            listClassName={styles.langList}
-          />
-          {userStore.user !== null ? (
-            <AccountMenu className={styles.accountMenu}>
-              <ProfileInfo
-                user={userStore.user}
-                first="name"
-                avatarHeightPX={40}
-                text={{ fontWeight: '400', fontSize: '16px', color: '#FFF' }}
-              />
-            </AccountMenu>
-          ) : (
-            <Button callback={toAuthorizePage} type="default">
-              <TranslatedMessage
-                message={Translation.translatedText('btn.signIn')}
-              />
-            </Button>
-          )}
-        </div> */}
+        {isBurger ? <BurgerMenu /> : <Navigation />}
       </div>
     </header>
   );
-});
+};
